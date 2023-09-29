17026b4b3b5bb2004c32a25fa22469eddd6b3e35
BRIAN MAHER <bmaher22@mac.comclear>
2023-09-28 23:44:34 -0400

    fixed questions,some js, highschores js, highscores html

---
+++ Assets/css/questions.js
@@ -0,0 +1,38 @@
+// Questions, options, and answers to the quiz.
+var questions = [
+    {
+        question: "WWW in a url stands for?",
+        answers: ["World Wide Wrestling", "Wide world of Websites", "World Wide Web", "Willy Wonka's Wishbone"],
+        correctAnswer: "World Wide Web"
+    },
+    {
+        question: "In the terminal, the ~ represents",
+        answers: ["Home", "Here", "Navigation", "Nothing"],
+        correctAnswer: "Home"
+    },
+    {
+        question: "What does HTML stand for?",
+        answers: ["hide-text-markup-language", "hyper-text-markup-language", "hyper-tab-market-language", "hyper-text-marketing-language"],
+        correctAnswer: "hyper-text-markup-language"
+    },
+    {
+        question: "What does JS refer to?",
+        answers: ["Java Syntax", "Joe Smith", "JavaScript", "Javier Sosa"],
+        correctAnswer: "JavaScript"
+    },
+    {
+        question: 'Javascript is an _______ language?',
+        answers: ['object-oriented', 'object-based', 'procedural', 'none of the above'],
+        correctAnswer: 'object-oriented'
+    },
+    {
+        question: "Which defines CSS?",
+        answers: ["central station surveillance", "cascading style sheet", "cascading short style"],
+        correctAnswer: "cascading style sheet"
+    },
+    {
+        question: "How do we write INDEX.html shorthand?",
+        answers: ["! tab", "index tab", "html tab", "there is no shorthand"],
+        correctAnswer: "! tab"
+    }
+];
---
+++ Assets/css/script.js
@@ -0,0 +1,169 @@
+// Assignment Code
+var startBtn = document.querySelector("#start-quiz");
+var submitScoreBtn = document.querySelector('#submit-score');
+var startPage = document.querySelector("#start-page");
+var questionPage = document.querySelector("#question-page");
+var resultPage = document.querySelector('#result-popup')
+var gameOverPage = document.querySelector('#game-over');
+var timeLeft = document.querySelector('#timer');
+
+// Global variables
+var questionsAnswered = 0;
+var score = 0;
+var gameTimer = 0;
+var timerInterval;
+
+// When the user clicks the start button, then the timer count down from 60.
+
+function displayStartPage() {
+    startPage.classList.remove('hidden');
+    questionPage.classList.add('hidden');
+    gameOverPage.classList.add('hidden');
+}
+
+function displayQuestionPage() {
+    questionPage.classList.remove('hidden');
+    startPage.classList.add('hidden');
+    gameOverPage.classList.add('hidden');
+}
+
+function displayGameOverPage() {
+    gameOverPage.classList.remove('hidden');
+    startPage.classList.add('hidden');
+    questionPage.classList.add('hidden');
+}
+
+function hideResultPopup() {
+    resultPage.classList.add("hidden");
+}
+
+function generateQuestion() {
+    var answerBtnContainer = document.querySelector("#answer-buttons");
+    answerBtnContainer.innerHTML = '';
+
+    var newQuestion = document.querySelector("#question");
+    var questionIndex = 1;
+    newQuestion.innerText = questions[questionsAnswered].question;
+
+    // when the user clicks the start button, the textbox dissappears, and the question-box will appear.
+
+    questions[questionsAnswered].answers.forEach((answer) => {
+        if (answer!==undefined) {
+            // Access properties of answer here
+        var answerBtn = document.createElement('button');
+        answerBtn.id = `#answer-btn-${questionIndex}`;
+        answerBtn.classList.add('answer-btn');
+ }
+        var text = document.createTextNode(`${questionIndex}. ${answer}`);
+        answerBtn.appendChild(text);
+        answerBtnContainer.appendChild(answerBtn);
+
+        // Add the button's event listener
+        answerBtn.addEventListener("click", answerClicked);
+        questionIndex++;
+
+    });
+}
+function gameOver() {
+    displayGameOverPage();
+
+    var finalScore = document.querySelector('#final-score-text');
+    finalScore.innerText = `You final score is ${score}.`;
+
+    questionsAnswered = 0;
+}
+
+function checkAnswer(answer) {
+    var resultText = document.querySelector('#result-text');
+    var result = '';
+
+    if (answer === questions[questionsAnswered].correctAnswer) {
+        result = 'Correct!';
+        score++;
+    } else {
+        result = 'Wrong!';
+        gameTimer -= 10;
+    }
+
+    if (gameTimer < 0) {
+        gameTimer = 0;
+    }
+
+    resultText.innerText = result;
+    resultPage.classList.remove('hidden');
+    setTimeout(hideResultPopup, 1500);
+
+    questionsAnswered++;
+
+    if (gameTimer === 0 || questionsAnswered === questions.length) {
+        setTimeout(gameOver, 1000);
+        gameTimer = 0;
+        timeLeft.textContent = gameTimer;
+        clearInterval(timerInterval);
+    } else {
+        generateQuestion();
+    }
+}
+
+function submitScore() {
+    var initials = document.querySelector('#initials');
+
+    var player = {
+        name: initials.value,
+        highScore: score
+    };
+
+    if (initials.value.length !== 0) {
+        addPlayerToStorage(player);
+        initials.value = '';
+    }
+
+    displayStartPage();
+}
+
+function addPlayerToStorage(player) {
+    const playersFromStorage = getPlayersFromStorage();
+
+    // Adds a new player to array
+    playersFromStorage.push(player);
+
+    // Converting to JSON string and set it to local storage
+    localStorage.setItem('player', JSON.stringify(playersFromStorage));
+}
+
+function getPlayersFromStorage() {
+    return JSON.parse(localStorage.getItem('player')) || [];
+}
+
+function startTimer() {
+    // Sets interval in variable
+    timerInterval = setInterval(function () {
+        gameTimer--;
+        timeLeft.textContent = gameTimer;
+
+        if (gameTimer === 0) {
+            clearInterval(timerInterval);
+            gameOver();
+        }
+
+    }, 1000);
+}
+
+function startQuiz(e) {
+
+    questionsAnswered = 0;
+    gameTimer = 60;
+    score = 0;
+    startTimer();
+    generateQuestion();
+    displayQuestionPage();
+}
+// Button Click Event for the answer buttons  
+function answerClicked(e) {
+    var answer = e.target.innerText.slice(3);
+    checkAnswer(answer);
+}
+
+// Add event listener to generate button events
+startBtn.addEventListener("click", startQuiz);
+submitScoreBtn.addEventListener("click", submitScore);
